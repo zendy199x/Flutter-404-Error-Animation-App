28d6415e73d7fe87a211586d53181282f5dfa729
Zendy <nguyenduc.051194@gmail.com>
2021-08-24 21:00:04 +0700

    404 error page animation

--- lib/screens/error/components/body.dart
+++ lib/screens/error/components/body.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:error_page_animation/screens/error/components/error_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -9,7 +11,33 @@ class Body extends StatefulWidget {
   _BodyState createState() => _BodyState();
 }
 
-class _BodyState extends State<Body> {
+class _BodyState extends State<Body> with SingleTickerProviderStateMixin {
+  final Duration duration = const Duration(milliseconds: 500);
+
+  bool startAnimation = false;
+  bool startBroke = false;
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    )..addListener(() {
+        setState(() {});
+      });
+    Future.delayed(const Duration(milliseconds: 100), () {
+      setState(() {
+        startAnimation = true;
+      });
+    });
+
+    Future.delayed(const Duration(milliseconds: 700), () {
+      _animationController!.forward();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -28,47 +56,63 @@ class _BodyState extends State<Body> {
                   width: 641,
                   height: 310,
                   child: Stack(
+                    clipBehavior: Clip.none,
                     children: [
                       // Shadow
-                      Positioned(
+                      AnimatedPositioned(
+                        duration: duration,
                         top: 0,
-                        left: 0,
-                        child: SvgPicture.asset("assets/icons/Group 64.svg"),
+                        left: startAnimation ? 0 : 50,
+                        child: AnimatedOpacity(
+                            opacity: startAnimation ? 1 : 0.3,
+                            duration: duration,
+                            child:
+                                SvgPicture.asset("assets/icons/Group 64.svg")),
                       ),
                       // Cycle
-                      Positioned(
+                      AnimatedPositioned(
+                        duration: duration,
                         bottom: 0,
-                        left: 130,
-                        child: SizedBox(
-                          height: 310,
-                          width: 528,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                bottom: 7,
-                                left: 230,
-                                child: SvgPicture.asset(
-                                  "assets/icons/cycle_part_2.svg",
-                                  height: 142,
-                                  width: 142,
+                        left: startAnimation ? 130 : 0,
+                        child: AnimatedOpacity(
+                          duration: duration,
+                          opacity: startAnimation ? 1 : 0,
+                          child: SizedBox(
+                            height: 310,
+                            width: 528,
+                            child: Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Positioned(
+                                  // duration: Duration(milliseconds: 200),
+                                  bottom: 7,
+                                  left: 230 + _animationController!.value * 90,
+                                  child: SvgPicture.asset(
+                                    "assets/icons/cycle_part_2.svg",
+                                    height: 142,
+                                    width: 142,
+                                  ),
                                 ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                // left: 120,
-                                child: SvgPicture.asset(
-                                  "assets/icons/cycle_part_1.svg",
-                                  height: 287,
+                                Positioned(
+                                  bottom: 0,
+                                  child: Transform.rotate(
+                                    angle: (_animationController!.value * 5) *
+                                        (pi / 180),
+                                    child: SvgPicture.asset(
+                                      "assets/icons/cycle_part_1.svg",
+                                      height: 287,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                              // Bottom Line
-                              Positioned(
-                                left: 0,
-                                bottom: 0,
-                                child:
-                                    SvgPicture.asset("assets/icons/line.svg"),
-                              )
-                            ],
+                                // Bottom Line
+                                Positioned(
+                                  left: 0,
+                                  bottom: 0,
+                                  child:
+                                      SvgPicture.asset("assets/icons/line.svg"),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -83,4 +127,3 @@ class _BodyState extends State<Body> {
     );
   }
 }
-
