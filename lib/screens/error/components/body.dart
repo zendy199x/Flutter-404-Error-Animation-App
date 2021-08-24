c833d75912f00919515f4660be7a32e4ae9ed171
Zendy <nguyenduc.051194@gmail.com>
2021-08-24 20:34:57 +0700

    404 page

--- lib/screens/error/components/body.dart
+++ lib/screens/error/components/body.dart
@@ -1,4 +1,6 @@
+import 'package:error_page_animation/screens/error/components/error_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -10,8 +12,75 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const ErrorText(),
+                SizedBox(
+                  width: 641,
+                  height: 310,
+                  child: Stack(
+                    children: [
+                      // Shadow
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: SvgPicture.asset("assets/icons/Group 64.svg"),
+                      ),
+                      // Cycle
+                      Positioned(
+                        bottom: 0,
+                        left: 130,
+                        child: SizedBox(
+                          height: 310,
+                          width: 528,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                bottom: 7,
+                                left: 230,
+                                child: SvgPicture.asset(
+                                  "assets/icons/cycle_part_2.svg",
+                                  height: 142,
+                                  width: 142,
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                // left: 120,
+                                child: SvgPicture.asset(
+                                  "assets/icons/cycle_part_1.svg",
+                                  height: 287,
+                                ),
+                              ),
+                              // Bottom Line
+                              Positioned(
+                                left: 0,
+                                bottom: 0,
+                                child:
+                                    SvgPicture.asset("assets/icons/line.svg"),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
---
+++ lib/screens/error/components/error_text.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ErrorText extends StatelessWidget {
+  const ErrorText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(
+          "assets/icons/error_text.svg",
+          height: 395,
+        ),
+        const SizedBox(height: 30),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 50,
+              vertical: 25,
+            ),
+            shape: const RoundedRectangleBorder(
+              side: BorderSide(
+                color: Color(0xFF293047),
+                width: 2,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
+          ),
+          onPressed: () {},
+          child: Text(
+            "Back To Home".toUpperCase(),
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
